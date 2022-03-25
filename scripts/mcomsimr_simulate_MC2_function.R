5a9a0c2fcb288c4ae5161b20dea028062ad6c214
James Hagan <james_hagan@outlook.com>
2022-03-25 13:49:16 +0100

    update the function to add carrying capacities

--- scripts/mcomsimr_simulate_MC2_function.R
+++ scripts/mcomsimr_simulate_MC2_function.R
@@ -1,8 +1,8 @@
 
 # Modified version of the simulate_MC function
 
-simulate_MC2 <- function(patches, species, dispersal = 0.01,
-                         plot = TRUE, timesteps = 1200, burn_in = 800, initialization = 200,
+simulate_MC2 <- function(patches, species, dispersal = 0.01, timesteps = 1200,
+                         start_abun = 500,
                          extirp_prob = 0,
                          landscape, disp_mat, env.df, env_traits.df, int_mat){
   
@@ -10,19 +10,24 @@ simulate_MC2 <- function(patches, species, dispersal = 0.01,
   library(ggplot2)
   
   dynamics.df <- data.frame()
-  N <- matrix(rpois(n = species*patches, lambda = 0.5), nrow = patches, ncol = species)
-  pb <- txtProgressBar(min = 0, max = initialization + burn_in + timesteps, style = 3)
-  for(i in 1:(initialization + burn_in + timesteps)){
-    if(i <= initialization){
-      if(i %in% seq(10,100, by = 10)){
-        N <- N + matrix(rpois(n = species*patches, lambda = 0.5), nrow = patches, ncol = species)
-      }
-      env <- env.df$env1[env.df$time == 1]
-    } else {
-      env <- env.df$env1[env.df$time == (i-initialization)]
-    }
-    r <- env_traits.df$max_r[1]*exp(-(t((env_traits.df$optima - matrix(rep(env, each = species), nrow = species, ncol = patches))/(2*env_traits.df$env_niche_breadth)))^2)
-    N_hat <- N*r/(1+N%*%int_mat)
+  N <- matrix(rep(round(start_abun/species, 0), species*patches), nrow = patches, ncol = species)
+  pb <- txtProgressBar(min = 0, max = timesteps, style = 3)
+  for(i in 1:(timesteps)){
+    
+    # get the first environmental condition
+    env <- env.df$env1[env.df$time == (i)]
+    
+    # we use the equation 3 to determine the realised growth rate
+    # of each species (col) in each patch (row)
+    r <- env_traits.df$max_r*exp(-(t((env_traits.df$optima - matrix(rep(env, each = species), nrow = species, ncol = patches))/(2*env_traits.df$env_niche_breadth)))^2)
+    
+    # we use equation 3 to determine the realised carrying capacity
+    # of each species (col) in each patch (row)
+    K <- env_traits.df$K_max*exp(-(t((env_traits.df$optima - matrix(rep(env, each = species), nrow = species, ncol = patches))/(2*env_traits.df$env_niche_breadth)))^2)
+    
+    # here we implement the difference equation
+    N_hat <- N + ((N*r) * (1 - ((N %*% int_mat)/K) ))
+    
     N_hat[N_hat < 0] <- 0
     N_hat <- matrix(rpois(n = species*patches, lambda = N_hat), ncol = species, nrow = patches)
     
@@ -40,31 +45,18 @@ simulate_MC2 <- function(patches, species, dispersal = 0.01,
     N <- N_hat - E + I
     N[rbinom(n = species * patches, size = 1, prob = extirp_prob)>0] <- 0
     
-    dynamics.df <- rbind(dynamics.df, data.frame(N = c(N), patch = 1:patches, species = rep(1:species, each = patches), env = env, time = i-initialization-burn_in))
+    dynamics.df <- rbind(dynamics.df, data.frame(N = c(N), patch = 1:patches, species = rep(1:species, each = patches), env = env, time = i))
     setTxtProgressBar(pb, i)
   }
   close(pb)
   dynamics.df <- dplyr::left_join(dynamics.df, env_traits.df, by = "species")
-  env.df$time_run <- env.df$time - burn_in
+  dynamics.df <- 
+    dynamics.df %>%
+    filter(time >= 0) %>%
+    select(time, patch, env, species, N)
   
-  env.df_init <- data.frame(env1 = env.df$env1[env.df$time == 1], patch = 1:patches, time = NA, time_run = rep(seq(-(burn_in + initialization), -burn_in, by = 1), each = patches))
-  env.df <- rbind(env.df_init,env.df)
+  return(dynamics.df)
   
-  if(plot == TRUE){
-    sample_patches <- sample(1:patches, size = min(c(patches,6)), replace = FALSE)
-    g <- dynamics.df %>%
-      filter(time %in% seq(min(dynamics.df$time),max(dynamics.df$time), by =10)) %>%
-      filter(patch %in% sample_patches) %>%
-      ggplot(aes(x = time, y = N, group = species, color = optima))+
-      geom_line()+
-      facet_wrap(~patch)+
-      scale_color_viridis_c()+
-      geom_path(data = filter(env.df, patch %in% sample_patches), aes(y = -5, x = time_run, color = env1, group = NULL), size = 3)
-    
-    print(g)
-  }
-  
-  return(list(dynamics.df = dynamics.df, landscape = landscape, env.df = env.df, env_traits.df = env_traits.df, disp_mat = disp_mat, int_mat = int_mat))
 }
 
 ### END
--- scripts/simulate_metacommunities.R
+++ scripts/simulate_metacommunities.R
@@ -126,17 +126,17 @@ sim_metacomm_BEF <- function(species = 5, patches = 10,
 # write a function to scale a variable between any two values
 # https://stats.stackexchange.com/questions/281162/scale-a-number-between-a-range
 normalise_x <- function(x, b, a) {
-  y <- (b - a) * ((x - min(x))/(max(x) - min(e.1$env1))) + a
+  y <- (b - a) * ((x - min(x))/(max(x) - min(x))) + a
   return(y)
 }
 
-  
+
 # set-up fixed inputs
-species <- 10
-patches <- 20
+species <- 3
+patches <- 3
 ts <- 100
 bi <- 100
-init <- 50
+init <- 100
 
 # generate a random landscape
 l.1 <- landscape_generate(patches = patches, plot = FALSE)
@@ -146,21 +146,82 @@ d.1 <- dispersal_matrix(landscape = l.1,
                         torus = TRUE,
                         kernel_exp = 0.1, plot = FALSE)
 
-# generate species interaction matrix randomly
-si.1 <- species_int_mat(species, intra = 1, min_inter = 0, 
-                        max_inter = 1.5, comp_scaler = 0.3, plot = FALSE)
-head(si.1)
-
-# generate a random environmental matrix
-e.1 <- env_generate(landscape = l.1, env1Scale = 900, timesteps = (ts + bi + init), plot = FALSE )
-hist(e.1$env1)
-
 # generate species environmental optima
-t.1 <- env_traits(species = species, max_r = 5, 
+t.1 <- env_traits(species = species, max_r = 0.5, 
                   min_env = 0, max_env = 1, 
                   env_niche_breadth = 0.2, 
                   plot = FALSE, optima_spacing = "even")
+t.1$K_max <- 150
 head(t.1)
+
+
+## Competition matrices
+
+# generate species interaction matrix randomly
+si.1 <- species_int_mat(species, intra = 1, min_inter = 0, 
+                        max_inter = 0.8, comp_scaler = 1, plot = FALSE)
+head(si.1)
+
+# generate neutral competition matrix
+si.n <- species_int_mat(species, intra = 1, min_inter = 1, 
+                        max_inter = 1, comp_scaler = 1, plot = FALSE)
+head(si.n)
+
+
+## environmental matrices
+
+# generate a random environmental matrix
+e.1 <- env_generate(landscape = l.1, env1Scale = 900, timesteps = (ts + bi + init), plot = FALSE )
+head(e.1)
+hist(e.1$env1)
+
+# environment space
+env.1 <- runif(n = patches, min = 0, max = 1)
+env.1 <- c(0.1, 0.5, 0.9)
+e.s <- expand.grid(1:patches, 1:(ts + bi + init))
+e.s$env1 <- rep(env.1,(ts + bi + init) ) 
+names(e.s) <- c("patch", "time", "env1")
+e.s <- e.s[,c("env1", "patch", "time")] 
+head(e.s)
+
+# environment time
+env.1 <- runif(n = (ts + bi + init), min = 0, max = 1)
+e.t <- expand.grid(1:patches, 1:(ts + bi + init))
+e.t$env1 <- rep(env.1, each = patches ) 
+names(e.t) <- c("patch", "time", "env1")
+e.t <- e.t[,c("env1", "patch", "time")] 
+
+# test the new metacommunity simulation function
+x <- 
+  simulate_MC2(patches = patches, species = species, dispersal = 0.1, 
+               timesteps = ts, start_abun = 150,
+               extirp_prob = 0.0001,
+               landscape = l.1, disp_mat = d.1, env.df = e.s, 
+               env_traits.df = t.1, int_mat = si.1)
+head(x)
+
+ggplot(data = x %>% mutate(species = as.character(species)),
+       mapping = aes(x = time, y = N, colour = species)) +
+  geom_line() +
+  facet_wrap(~patch, scales = "free") +
+  theme_classic()
+
+
+## Run examples to test if the outputs from the function make sense
+sim_metacomm_BEF(species = species, 
+                 patches = patches, 
+                 dispersal = 0.2, 
+                 timesteps = ts, burn_in = bi, initialization = init,
+                 extirp_prob = 0.0001,
+                 landscape = l.1, 
+                 disp_mat = d.1, 
+                 env.df = e.s, 
+                 env_traits.df = t.1, 
+                 int_mat = si.1)
+
+
+
+
 
 # set number of replicates: 10
 n_rep <- 100
