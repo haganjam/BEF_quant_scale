1076a46c0bee505aa57b226cec692647feb1edf3
James Hagan <james_hagan@outlook.com>
2022-06-30 15:00:10 +0200

    add markdown with frame templates

--- scripts/03_empirical_analysis/02_fieldwork_2022/01_preparation/01_randomise_mix_mono_position.R
+++ scripts/03_empirical_analysis/02_fieldwork_2022/01_preparation/01_randomise_mix_mono_position.R
@@ -16,6 +16,10 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(googlesheets4)
+library(here)
+
+# load plotting function
+source(here("scripts/Function_plotting_theme.R"))
 
 # read a Google sheet
 pan_dat <- read_sheet("https://docs.google.com/spreadsheets/d/1fUn4k0SuZMr7KVWW1PPDzviemPAKGBNtelKgJl11lXU/edit#gid=0", 
@@ -43,42 +47,59 @@ mix_mono <- dplyr::bind_rows(mix_mono)
 pan_dat <- dplyr::bind_cols(clus_lab, mix_mono)
 View(pan_dat)
 
+# plot a grid for each site_id
+lapply( split(pan_dat, pan_dat$site_id), function(z) {
+  
+  x <- z[, c(3, 5, 6, 7)]
+  
+  x$row <- rep(c(3, 2, 1), each = 4)
+  x$col <- rep(c(1, 2, 3, 4), 3)
+  
+  # add a column for species
+  x$species <- "species =                         "
+  x$species <- ifelse(x$time_point != "NA", 
+                      "", x$species)
+  
+  # replace NAs with blank data
+  x$time_point <- ifelse(x$time_point == "NA", "time =        ", 
+                         paste("time =", x$time_point) ) 
+  
+  # plot the grid
+  p1 <- 
+    ggplot(data = x,
+           mapping = aes(x = col, y = row, fill = treatment)) +
+    geom_tile(colour = "black") +
+    geom_label(mapping = aes(label = panel_id), 
+               label.padding = unit(0.25, "lines"), label.size = NA,
+               vjust = -2, size = 5.5) +
+    geom_label(mapping = aes(label = time_point), 
+               label.padding = unit(0.25, "lines"), label.size = NA,
+               vjust = -0.25, size = 5.5) +
+    geom_label(mapping = aes(label = treatment), 
+               label.padding = unit(0.25, "lines"), label.size = NA,
+               vjust = 1.5, size = 5.5) +
+    geom_label(mapping = aes(label = species), 
+               label.padding = unit(0.25, "lines"), label.size = NA,
+               vjust = 3.25, size = 5.5) +
+    scale_fill_manual(values = c("grey", "white")) +
+    theme(legend.position = "none") +
+    ylab(NULL) +
+    xlab(NULL) +
+    ggtitle(label = paste("site_id",  x$site_id[1], sep = " - ") ) +
+    theme(plot.title = element_text(hjust = 0.5, size = 20)) +
+    #theme_meta() +
+    theme(axis.line=element_blank(),axis.text.x=element_blank(),
+          axis.text.y=element_blank(),axis.ticks=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank(),legend.position="none",
+          panel.background=element_blank(),panel.border=element_blank(),
+          panel.grid.major=element_blank(), panel.grid.minor=element_blank() )
+  
+  # export the grid to a .csv file
+  ggsave(filename = here(paste("scripts/03_empirical_analysis/02_fieldwork_2022/01_preparation/template_", 
+                               x$site_id[1], ".png", sep = "")),
+         p1, width = 29, height = 20, units = "cm", dpi = 300)
+  
+} )
 
-x <- pan_dat[c(1:12), ]
-x <- x[, c(3, 5, 6, 7)]
-head(x)
-
-x$row <- rep(c(1, 2, 3), each = 4)
-x$col <- rep(c(1, 2, 3, 4), 3)
-
-# replace NAs with blank data
-x$time_point <- ifelse(x$time_point == "NA", "", x$time_point)
-
-# add a column for species
-x$species <- "species =                         "
-x$species <- ifelse(x$time_point != "", "", x$species)
-
-ggplot(data = x,
-       mapping = aes(x = col, y = row, fill = treatment)) +
-  geom_tile(colour = "black") +
-  geom_label(mapping = aes(label = panel_id), 
-             label.padding = unit(0.25, "lines"), label.size = NA,
-             vjust = -1.75) +
-  geom_label(mapping = aes(label = time_point), 
-             label.padding = unit(0.25, "lines"), label.size = NA,
-             vjust = -0.25) +
-  geom_label(mapping = aes(label = treatment), 
-             label.padding = unit(0.25, "lines"), label.size = NA,
-             vjust = 1.25) +
-  geom_label(mapping = aes(label = species), 
-             label.padding = unit(0.25, "lines"), label.size = NA,
-             vjust = 2.75) +
-  scale_fill_manual(values = c("grey", "white")) +
-  theme(legend.position = "none") +
-  ylab(NULL) +
-  xlab(NULL) +
-  ggtitle(label = paste("site_id",  x$site_id[1], sep = " - ") ) +
-  theme(plot.title = element_text(hjust = 0.5, size = 18))
-
-
-
+### END
