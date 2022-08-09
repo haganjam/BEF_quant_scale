168e776a0b6341968eec7b99398a70d85a103e86
James Hagan <james_hagan@outlook.com>
2022-08-09 10:10:14 +0200

    update the summary script

--- scripts/02_simulation/05_analysis_test_summarise.R
+++ scripts/02_simulation/05_analysis_test_summarise.R
@@ -71,40 +71,36 @@ BEF_sum_list <- foreach(
   
   # calculate the overall monoculture error
   
-  # summarise the posterior distribution into a mean
+  # summarise the posterior distribution into a mean and PI range
   mu_m <- apply(MC_sims2[[i]] [["MC.x.pred"]], 2, function(x) mean(x) )
+  
+  mu_PI <- 
+    apply(MC_sims2[[i]] [["MC.x.pred"]], 2, function(z) {
+      
+      range_out <- quantile(z, 0.95) - quantile(z, 0.05)
+      
+      return(range_out)
+      
+    } )
+  
+  mu_PI <- (abs(mu_m - mu_PI)/mu_m)*100
+  
+  # summarise the monoculture PI, correlation etc.
   
   # calculate the correlation coefficient between the mean of the posterior and the true monoculture values
   BEF_sum[["mono_cor"]] <- cor(mu_m, MC_sims2[[i]] [["MC.x.NA"]]$M[is.na(MC_sims2[[i]] [["MC.x.NA"]]$M1)])
   
-  # calculate absolute error between observed and modelled monocultures
-  mono_error <- 
-    
-    apply(MC_sims2[[i]] [["MC.x.pred"]], 1, function(x) {
-      
-      M_obs <- MC_sims2[[i]] [["MC.x.NA"]] [is.na(MC_sims2[[i]] [["MC.x.NA"]][["M1"]]), ] [["M"]]
-      sum( abs( x - M_obs ) )
-      
-    })
+  # calculate the proportion of monocultures where the range is greater than 100% of the mean
+  BEF_sum[["prop_wide_mono"]] <- sum(ifelse(mu_PI_range > 100, 1, 0))/length(mu_PI_range)
   
-  BEF_sum[["mono_error"]] <- mean(mono_error, na.rm = TRUE)
-  
-  # calculate the average PI range around the monocultures
-  PI_m <- apply(MC_sims2[[i]] [["MC.x.pred"]], 2, function(x) { 
-    
-    PI_range <- (quantile(x, 0.95) - quantile(x, 0.05))
-    
-    return(PI_range) } 
-    
-  )
-  
-  BEF_sum[["mono_PI_range"]] <- mean(PI_m, na.rm = TRUE)
+  # calculate the maximum monoculture range size from the mean as a percentage
+  BEF_sum[["max_mono_width"]] <- max(mu_PI)
   
   # reorder the columns
   BEF_sum <- 
     
     BEF_sum %>%
-    select(t_steps, dispersal, start_abun, optima, niche_breadth, inter_comp, mono_cor, mono_error, mono_PI_range,
+    select(t_steps, dispersal, start_abun, optima, niche_breadth, inter_comp, mono_cor, prop_wide_mono, max_mono_width,
            Beff, Value_obs, mu,
            PI_low, PI_high)
   
