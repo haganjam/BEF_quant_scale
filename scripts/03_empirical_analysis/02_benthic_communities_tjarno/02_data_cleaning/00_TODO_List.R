a99b37bb2dd17258495969e5a3c7fc4798149923
bschrobru <bschrobru@gmail.com>
2022-10-01 13:43:45 +0200

    add todo list

---
+++ scripts/03_empirical_analysis/02_benthic_communities_tjarno/02_data_cleaning/00_TODO_List.R
@@ -0,0 +1,30 @@
+
+
+#download from research box and create file structure
+
+#1_download_data_from_researchbox
+
+#folders:
+light_temperature_logger_data
+light_temperature_logger_sensor_correction_data
+raw_data
+
+#2_cleaning_light_sensor_correction
+
+#create lm to correct light sensor with light sensor data
+
+#3_cleaning_light_temperature_logger
+
+#add combined table to data/analysis_data
+
+#4_cleaning_environmental_data
+#-summarize light temp data
+#-gypsum data
+#-abiotic field measurements
+
+#add combined table to data/analysis_data
+
+#5_cleaning_biomass_data
+#-clean biotic data
+
+
