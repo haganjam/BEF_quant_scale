d79ae3dc36c95a4a62d0709faab26889088958c7
bschrobru <bschrobru@gmail.com>
2022-10-06 17:23:50 +0200

    unzipper

--- scripts/03_empirical_analysis/02_benthic_communities_tjarno/02_data_cleaning/1_download_data_from_researchbox.R
+++ scripts/03_empirical_analysis/02_benthic_communities_tjarno/02_data_cleaning/1_download_data_from_researchbox.R
@@ -1,10 +1,22 @@
+#'
+#' @title: Unzip the ResearchBox data and save it in the current directory
+#' 
+#' @description: 
+#' 
+#' 1. Download the data from https://researchbox.org/843&PEER_REVIEW_passcode=GLGJFF
+#' 2. The script will unzip the fill and add it to the project directory
+#' 3. You can also unzip and add it manually to "[PROJECT]/data/benthic_communities_data"
+#' @authors: James G. Hagan (james_hagan(at)outlook.com)
+#' 
+#' 
+#' 
 
-#TODO
-#create folder structure in /data:
 
-benthic_communities_data
-#sub_folders:
-light_temperature_logger_data
-light_temperature_logger_sensor_correction_data
-raw_data
-analysis_data
+# load relevant libraries
+require(here)
+
+# unzip the file
+unzip(zipfile = file.choose(),
+      exdir = here("data/benthic_communities_data"))
+### END
+
