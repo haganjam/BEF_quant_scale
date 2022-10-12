d07b805feefb02e19b1d44e2455b8d379f294b05
James Hagan <james_hagan@outlook.com>
2022-10-12 15:30:24 +0200

    update the cleaning scripts

--- scripts/03_empirical_analysis/02_benthic_communities_tjarno/02_data_cleaning/04_cleaning_environmental_data.R
+++ scripts/03_empirical_analysis/02_benthic_communities_tjarno/02_data_cleaning/04_cleaning_environmental_data.R
@@ -1,16 +1,14 @@
 #'
 #' @title: Clean environmental data
 #' 
-#' @description: 
-#' Cleaning of abiotic field measurements
+#' @description: Cleaning of abiotic field measurements
 #' In the end a combined dataset for abiotic measures, flow speed, 
 #' light temperature, depth will be created
-#' 
-#' This script will require the raw hobologger data from Researchbox
 #' 
 #' @authors: Lara Martins, Benedikt Schrofner-Brunner
 #' 
 
+# load required libraries
 library(readxl)
 library(dplyr)
 library(tidyr)
@@ -20,401 +18,157 @@ library(here)
 library(stringr)
 library(lubridate)
 
-#Load cleaning functions
+# load cleaning functions
 source(here("scripts/03_empirical_analysis/02_benthic_communities_tjarno/02_data_cleaning/cleaning_functions.r"))
 
+# check if the correct directories exist
+check.dirs() 
 
-check.dirs()# check if directory exists
-#Get filenames from researchbox table of contents
-files=get.data.filenames("raw data")
+# 1. load the abiotic measurement data
+abiotic_measurements <- 
+  read_csv(here("data/benthic_communities_tjarno_data/ResearchBox 843/Data/abiotic_field_measurements.csv"), 
+           col_types = cols(Date = col_date(format = "%d/%m/%Y"), 
+                            Depth = col_number(), Temp. = col_number(), 
+                            Salinity = col_number(), Oxygen = col_number(), 
+                            Secchi = col_number())
+           )
 
-abiotic_measurements_filename="abiotic_field_measurements.csv"
-
-data.folder = here("data/benthic_communities_tjarno_data/ResearchBox 843/Data/")
-cleaned.data.folder = here("data/benthic_communities_tjarno_data/data_clean/")
-
-#####Load Abiotic Measurements######
-abiotic_measurements <- read_csv(paste(data.folder,abiotic_measurements_filename,sep = ""), 
-                                       col_types = cols(Date = col_date(format = "%d/%m/%Y"), 
-                                                        Depth = col_number(), Temp. = col_number(), 
-                                                        Salinity = col_number(), Oxygen = col_number(), 
-                                                        Secchi = col_number()))
+# rename the columns
 names(abiotic_measurements) = c("date","cluster_id","site_id","panel_depth",
                                 "temperature_C_field", "salinity_ppt_field",
                                 "oxygen","secchi_depth_m","weather_comment","comment")
 
 # write t1, t2 and t3 in the table
-abiotic_measurements$time <- "" #Create the column empty
+abiotic_measurements$time <- "" 
 
-abiotic_measurements$time[abiotic_measurements$date <= '2022-08-16'] <- 't1' #chose the column, search for the specific date, write the "names" of that
+# add t1, t2 and t3 labels
+abiotic_measurements$time[abiotic_measurements$date <= '2022-08-16'] <- 't1'
 abiotic_measurements$time[(abiotic_measurements$date >= '2022-08-17') & (abiotic_measurements$date <= '2022-08-31')] <- 't2'
 abiotic_measurements$time[abiotic_measurements$date >= '2022-09-01'] <- 't3'
 
-hobos$time[hobos$date <= '2022-08-10'] <- 't1' #chose the column, search for the specific date, write the "names" of that
-hobos$time[(hobos$date >= '2022-08-10') & (hobos$date <= '2022-08-22')] <- 't2'
-hobos$time[hobos$date >= '2022-08-23'] <- 't3'
+# view the abiotic measurements data
+print(abiotic_measurements)
 
-#'TODOs
-#'Summarise Logger data
-#'Gypsum data
-#'Fieldwork measurements (secchi, temperature, )
-#'Site data (depth and lon lat)
-#'
+# summarise the abiotic measurements
+abiotic_measurements <- 
+  abiotic_measurements %>%
+  rename(depth = panel_depth) %>%
+  group_by(site_id, depth, time) %>%
+  summarise(temp_field_C_m = mean(temperature_C_field, na.rm = TRUE),
+            sal_field_ppt_m = mean(salinity_ppt_field, na.rm = TRUE),
+            oxy_field_m = mean(oxygen, na.rm = TRUE),
+            secchi_field_m = mean(secchi_depth_m, na.rm = TRUE),
+            .groups = "drop")
 
+# 2. load the cleaned logger data
 
+# load the abiotic measurement data
+hobo_dat <- 
+  read_csv(here("data/benthic_communities_tjarno_data/data_clean/light_temperature_logger_clean.csv"))
 
+# reorganise the columns
+hobo_dat <- 
+  hobo_dat %>%
+  select(site, id, time, date_time, depth, temp_C, lux.corrected) %>%
+  rename(site_id = site)
 
+# summarise the temperature and light data
+hobo_dat <- 
+  hobo_dat %>%
+  group_by(site_id, depth, time) %>%
+  summarise(temp_C_m = mean(temp_C, na.rm = TRUE),
+            temp_C_cv = (sd(temp_C, na.rm = TRUE)/mean(temp_C, na.rm = TRUE))*100,
+            temp_C_max = min(temp_C, na.rm = TRUE),
+            temp_C_min = max(temp_C, na.rm = TRUE),
+            lux_m = mean(lux.corrected, na.rm = TRUE),
+            lux_cv = (sd(lux.corrected, na.rm = TRUE)/mean(temp_C, na.rm = TRUE))*100,
+            lux_max = max(lux.corrected, na.rm = TRUE),
+            lux_min = min(lux.corrected, na.rm = TRUE),
+            .groups = "drop")
 
+# 3. load the gypsum data
+gypsum <- 
+  read_csv(here("data/benthic_communities_tjarno_data/ResearchBox 843/Data/water_velocity_gypsum.csv"),
+           col_types = cols(cluster = col_character(),
+                            site = col_character(),
+                            date_deploy = col_character(),
+                            time_deploy = col_character(),
+                            date_retrieve = col_character(),
+                            time_retrieve = col_character()
+                            ))
 
+# add the year to date deploy and retrieve columns
+gypsum$date_deploy <- as.character(dmy(paste0(gypsum$date_deploy, "-2022")))
+gypsum$date_retrieve <- as.character(dmy(paste0(gypsum$date_retrieve, "-2022")))
 
-
-
-
-
-
-
-
-#-summarize light temp data
-#-gypsum data
-#-abiotic field measurements
-
-
-
-
-
-#add combined table to data/analysis_data
-
-
-
-
-################################
-################################
-########LARA SCRIPTS############
-############START###############
-#############HERE###############
-################################
-
-######LARA SCRIPT 2######
-
-#Data preparation - Abiotic measurements
-
-# load relevant libraries
-library(readxl)
-library(dplyr)
-library(tidyr)
-library(readr)
-library(ggplot2)
-library(here)
-library(stringr)
-library(lubridate)
-
-setwd("~/Universidade/EstagioSuecia/RHobo")
-
-#Load data
-
-abiotic <- read_excel("abiotic_measurements_data.xlsx",  col_types = c("date","text", "text", "numeric", "numeric", "numeric", "numeric", "numeric", "text", "text"))
-
-abiotic <- abiotic[-83,] #delete weird days
-abiotic <- abiotic[-87,] #delete weird days
-abiotic <- abiotic[-99,] #delete weird days
-
-colnames(abiotic) <- c("date","cluster", "site", "depth", "abiotic_temp", "salinity", "oxygen", "secchi", "cloudiness", "comment")
-
-# # write t1, t2 and t3 in the table
-# abiotic$time <- "" #Create an empty column
-# 
-# abiotic$time[abiotic$date==ymd('2022-07-21'] <- 't1' #chose the column, search for the specific date, write the "names" of that
-# abiotic$time[abiotic$date=='2022-08-01'] <- 't2'
-# abiotic$time[abiotic$date=='2022-08-31'] <- 't3'
-
-abiotic_new = abiotic %>% 
-  mutate(time = case_when(
-    date == ymd("2022-07-20") ~ "t1",
-    date == ymd("2022-08-01") ~ "t2",
-    date == ymd("2022-08-31") ~ "t3"
-  ))
-
-abiotic_new <- abiotic_new[,c(1,2,3,11,5,6,7,8,9,10,4)]
-abiotic <- abiotic_new
-
-
-#save the data into csv in the folder
-write.csv(abiotic,"abiotic_measurement_compilation_clean.csv", row.names = FALSE)
-
-
-########LARA SCRIPT 3####
-
-
-#Data preparation - Gypsum data
-
-# load relevant libraries
-library(readxl)
-library(dplyr)
-library(tidyr)
-library(readr)
-library(ggplot2)
-library(here)
-library(stringr)
-library(lubridate)
-
-setwd("~/Universidade/EstagioSuecia/RHobo")
-
-#Load data
-gypsum <- read_excel("gypsum_measurement_compilation.xlsx", col_types = c("text", "text", "text", "text", "numeric", "numeric", "date", "date", "date", "date", "text", "numeric", "text"))
-
-#_____________________________________________________________________________
-
-#Delete NAs
+# remove the NAs in the gypsum data
 gypsum <- gypsum[complete.cases(gypsum$weight_after), ]
 
-
-#_____________________________________________________________________________
-
-#Separate time_deploy and time_retrieve -> only time
-gypsum[c("time_out1","time_deploy")] <- str_split_fixed(gypsum$time_deploy," ",2)
-
-gypsum[c("time_out2","time_retrieve")] <- str_split_fixed(gypsum$time_retrieve," ",2)
-gypsum <- gypsum[,-14] #delete the columns that we don't want
-gypsum <- gypsum[,-14] #delete the columns that we don't want
-
-#_____________________________________________________________________________
-#Join date_deploy with date_retrieve
-
-
-#filter the data "inside" the date and time
+# get date time columns
 gypsum <- 
   gypsum  %>%
-  mutate(date_time_deploy = ymd_hms(paste(date_deploy, time_deploy, sep = " "), tz = "Europe/Stockholm") )
-#mutate is going to add new variables. Join date and time to be easier to deal with that. tz = specific time zone
+  mutate(date_time_deploy = ymd_hm(paste(date_deploy, time_deploy, sep = " "), tz = "Europe/Stockholm"),
+         date_time_retrieve = ymd_hm(paste(date_retrieve, time_retrieve, sep = " "), tz = "Europe/Stockholm"))
+
+# calculate the duration that the gympsum blocks were out for
+gypsum <- 
+  gypsum %>%
+  mutate(duration_hours = as.numeric(date_time_retrieve - date_time_deploy))
+
+# get relevant columns
+names(gypsum)
 
 gypsum <- 
-  gypsum  %>%
-  mutate(date_time_retrieve = ymd_hms(paste(date_retrieve, time_retrieve, sep = " "), tz = "Europe/Stockholm") )
+  gypsum %>%
+  select(site, cluster, gypsum_id, panel_depth_m, date_deploy, weight_before, weight_difference, duration_hours) %>%
+  rename(site_id = site)
 
-#_____________________________________________________________________________
-#Subtract the time between them
+# calculate the reduction per hour
+gypsum <- 
+  gypsum %>%
+  mutate(reduction_g_hour = (weight_difference/weight_before)/duration_hours)
 
-gypsum_time <- tibble(started_at = gypsum$date_time_deploy, 
-                      ended_at = gypsum$date_time_retrieve) #"2020-05-27 10:03:52",
+# add time variables
+gypsum$time <- ""
 
-gypsum_time <- gypsum_time %>% 
-  mutate(started_at = as.POSIXct(started_at,format = "%Y-%m-%d %H:%M:%S"),
-         ended_at = as.POSIXct(ended_at,format = "%Y-%m-%d %H:%M:%S"))
-
-gypsum_time$ended_at - gypsum_time$started_at
-
-
-gypsum_time <-  gypsum_time %>% mutate(duration = ended_at - started_at)
-colnames(gypsum_time) <- c("date_time_deploy", "date_time_retrieve", "hours_in_field")
-
-gypsum_time<- tibble(gypsum_time) #transform the table difftime into a tibble
-
-#_____________________________________________________________________________
-#Merge gypsum_time and gypsum in the same dataset
-
-gypsum <- merge(gypsum,gypsum_time,by= c("date_time_deploy"))
-gypsum
-
-
-#Clean the data
-gypsum <- gypsum[,-(12)] 
-gypsum <- gypsum[,c(2,3,4,5,6,7,8,9,10,1,11)] #reorder the columns
-colnames(gypsum_time) <- c("date_time_deploy", "date_time_retrieve", "hours_in_field")
-gypsum <- gypsum[,c(1,2,3,4,5,6,7,8,10,9,11)]
-
-#_____________________________________________________________________________
-
-#Apply the formula percent reduction per hour -> weight difference/weight before/time
-gypsum$percent <- gypsum$weight_difference/gypsum$weight_before
-
-#hours_in_field.y are difftime class -> numeric
-gypsum$hours_in_field.y <- as.numeric(gypsum$hours_in_field.y)
-gypsum$reduction_hour <- gypsum$percent/gypsum$hours_in_field.y # / by time
-
-#_____________________________________________________________________________
-
-# Organize the time -> t1, t2 and t3
-
-gypsum$time <- "" #Create an empty column
-
-
+# convert the date deploy column to simply a date
 gypsum$date_deploy <- as.Date(gypsum$date_deploy)
 
+# add labels based on the time points
 gypsum$time[gypsum$date_deploy <='2022-07-21'] <- 't1' #chose the column, search for the specific date, write the "names" of that
 gypsum$time[(gypsum$date_deploy>='2022-08-02') & (gypsum$date_deploy <= '2022-08-29')] <- 't2'
 gypsum$time[gypsum$date_deploy>='2022-08-29'] <- 't3'
 
+# summarise the gypsum data
+gypsum <- 
+  gypsum %>%
+  rename(depth = panel_depth_m) %>%
+  group_by(site_id, depth, time) %>%
+  summarise(reduction_g_hour_m = mean(reduction_g_hour, na.rm = TRUE),
+            .groups = "drop")
 
+# 4. load the site data
+site_dat <- read_csv(file = here("data/benthic_communities_tjarno_data/ResearchBox 843/Data/site_data.csv"))
 
-#_____________________________________________________________________________
-#save the data into csv in the folder
-write.csv(gypsum,"gypsum_measurement_compilation_clean.csv", row.names = FALSE)
+# rename the depth variable
+site_dat <- 
+  site_dat %>%
+  rename(depth = panel_depth_m)
 
+# 5. combine the site data with all the other abiotic measurements
 
-##############LARA SCRIPT 4#####
+# merge the datasets
+env_dat <- 
+  left_join( 
+  left_join(hobo_dat, abiotic_measurements, by = c("site_id", "depth", "time")),
+  gypsum,
+  by = c("site_id", "depth", "time"))
 
+# join the env_dat to the site data
+site_dat <- right_join(site_dat, env_dat, by = c("site_id", "depth"))
 
-#Data summarize - Hobos
+# export this file into the data_clean folder
+write_csv(site_dat, here("data/benthic_communities_tjarno_data/data_clean/site_env_data.csv"))
 
-# load relevant libraries
-library(readxl)
-library(dplyr)
-library(tidyr)
-library(readr)
-library(ggplot2)
-library(here)
-library(stringr)
-library(lubridate)
-
-setwd("~/Universidade/EstagioSuecia/RHobo")
-
-#load tables
-hobos <- read_csv("hobos_measurement_compilation_clean.csv")
-abiotic <- read_csv("abiotic_measurement_compilation_clean.csv") 
-gypsum <- read_csv("gypsum_measurement_compilation_clean.csv")
-
-
-#Filter the time between (8h-20h) 
-#define time interval with date and time
-
-d19 <- ymd_hms("2022-07-07 00:00:00", tz = "Europe/Stockholm") #START day
-d20 <- ymd_hms("2022-07-07 23:00:00", tz = "Europe/Stockholm") 
-d <- ymd_hms("2022-07-27 08:00:00", tz = "Europe/Stockholm") #t0
-d2 <- ymd_hms("2022-07-27 20:00:00", tz = "Europe/Stockholm") 
-d3 <- ymd_hms("2022-07-28 08:00:00", tz = "Europe/Stockholm") 
-d4 <- ymd_hms("2022-07-28 20:00:00", tz = "Europe/Stockholm")
-d5 <- ymd_hms("2022-07-29 08:00:00", tz = "Europe/Stockholm") 
-d6 <- ymd_hms("2022-07-29 20:00:00", tz = "Europe/Stockholm")
-d7 <- ymd_hms("2022-08-08 08:00:00", tz = "Europe/Stockholm") #t1
-d8 <- ymd_hms("2022-08-08 20:00:00", tz = "Europe/Stockholm")
-d9 <- ymd_hms("2022-08-09 08:00:00", tz = "Europe/Stockholm") 
-d10 <- ymd_hms("2022-08-09 20:00:00", tz = "Europe/Stockholm")
-d11 <- ymd_hms("2022-08-17 08:00:00", tz = "Europe/Stockholm") #t2
-d12 <- ymd_hms("2022-08-17 20:00:00", tz = "Europe/Stockholm")
-d13 <- ymd_hms("2022-08-18 08:00:00", tz = "Europe/Stockholm") 
-d14 <- ymd_hms("2022-08-18 20:00:00", tz = "Europe/Stockholm")
-d15 <- ymd_hms("2022-09-01 08:00:00", tz = "Europe/Stockholm") #t3
-d16 <- ymd_hms("2022-09-01 20:00:00", tz = "Europe/Stockholm")
-d17 <- ymd_hms("2022-09-02 08:00:00", tz = "Europe/Stockholm") 
-d18 <- ymd_hms("2022-09-02 20:00:00", tz = "Europe/Stockholm") #END day
-
-d21 <- ymd_hms("2022-09-08 00:00:00", tz = "Europe/Stockholm")#akward days to delete
-d22 <- ymd_hms("2022-09-08 23:00:00", tz = "Europe/Stockholm")#akward days to delete
-d23 <- ymd_hms("2022-09-09 00:00:00", tz = "Europe/Stockholm")#akward days to delete
-d24 <- ymd_hms("2022-09-09 23:00:00", tz = "Europe/Stockholm")#akward days to delete
-
-
-#find the interval of sampling (loggers were outside of the water)
-#filter the opposite
-
-hobos <- 
-  hobos %>%
-  filter( !(date_time > d & date_time < d2) ) %>% 
-  filter( !(date_time > d3 & date_time < d4) )%>% 
-  filter( !(date_time > d5 & date_time < d6) )%>% 
-  filter( !(date_time > d7 & date_time < d8) )%>% 
-  filter( !(date_time > d9 & date_time < d10) )%>% 
-  filter( !(date_time > d11 & date_time < d12) )%>% 
-  filter( !(date_time > d13 & date_time < d14) )%>% 
-  filter( !(date_time > d15 & date_time < d16) )%>% 
-  filter( !(date_time > d17 & date_time < d18) )%>% 
-  filter( !(date_time > d19 & date_time < d20) )%>% 
-  filter( !(date_time > d21 & date_time < d22) )%>% 
-  filter( !(date_time > d23 & date_time < d24) )
-
-
-#___________________________________________________________________________
-
-
-
-### Mean and Coeficient of variation
-
-t1_temp <- hobos %>% 
-  filter(date_time<d7) %>% 
-  group_by(site) %>% 
-  summarise(mean_temp = mean(temp), cv_temp=sd(temp)/mean(temp)) #for the t1
-
-#CV = σ / μ 
-#cbind(t1_temp,"t1") not good ideia
-t1_temp$time <- "t1" #tibble first 10 rows/columns, variable type, and not weird numbers
-
-
-t2_temp <- hobos %>% 
-  filter(date_time>d7 & date_time<d11) %>% 
-  group_by(site) %>% 
-  summarise(mean_temp = mean(temp), cv_temp=sd(temp)/mean(temp)) #for the t2
-t2_temp$time <- "t2"
-
-
-
-t3_temp <- hobos %>% 
-  filter(date_time<d15) %>% 
-  group_by(site) %>% 
-  summarise(mean_temp = mean(temp), cv_temp=sd(temp)/mean(temp)) #for the t3
-t3_temp$time <- "t3"
-
-
-#Bind both tibbles
-stat_table <- bind_rows(t1_temp, t2_temp, t3_temp)
-
-
-
-# Mean of temp for all days per site
-
-ggplot(stat_table, aes(x=site, y=mean_temp)) +
-  geom_point(colour="#1F78B4") +
-  labs(x="Cluster",
-       y="Mean temperature (ºC)",
-       title = "Mean of temperature per site registered by the loggers") +
-  guides(x = guide_axis(angle = 90))
-#The mean looks to the temp change
-
-
-# Mean of temp and coeficient variation
-ggplot(stat_table, aes(x=site, y=cv_temp)) +
-  geom_point(colour="#1F78B4") +
-  labs(x="Cluster",
-       y="Mean temperature (ºC)",
-       title = "CV per site registered by the loggers") +
-  guides(x = guide_axis(angle = 90))
-#The coeficient variation looks to the stability of the values
-
-
-
-
-
-### Light Summary
-
-hobos_sum1 <- hobos %>%
-  group_by(site, time) %>%
-  summarise(light = mean(lux), sd = sd(lux), cv = (sd(lux)/mean(lux))) #and join the summarize of the abiotic
-
-
-
-
-#___________________________________________________________________________________
-
-#Data Combination - Hobos + abiotics-fieldwork + gypsum
-
-### Merge the two datasets into one - Hobos + Temperature
-
-hobos_sum <- hobos %>%
-  group_by(site, time) %>%
-  summarise(mean_temp = mean(temp), sd = sd(temp), cv = (sd(temp)/mean(temp))) #and join the summarize of the abiotic
-
-
-data = merge(hobos_sum,abiotic,by= c("time", "site"))
-data
-
-
-### Merge the two datasets into one - data + gypsum
-
-
-
-data = merge(data,gypsum,by= c("time", "site"))
-data
-
-
-#save the data into csv in the folder
-write.csv(abiotic,"main_data_comp_abiot_fieldwork.csv", row.names = FALSE)
+### END
 
