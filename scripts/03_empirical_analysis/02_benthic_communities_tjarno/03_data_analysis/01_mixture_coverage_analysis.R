7ab4a5d342a17ce52bb058fbfe95d3944756e4cf
James Hagan <james_hagan@outlook.com>
2022-10-12 15:55:45 +0200

    add a script to analyse the mixture-coverage data

---
+++ scripts/03_empirical_analysis/02_benthic_communities_tjarno/03_data_analysis/01_mixture_coverage_analysis.R
@@ -0,0 +1,9 @@
+#'
+#' @title: Analyse the cover correction data
+#' 
+#' @description: Script to clean the cover-correction data. This cover-correction
+#' data is used to check whether our weights taken at different time points
+#' are representative samples of the same communities.
+#' 
+#' @authors: James G. Hagan (james_hagan(at)outlook.com)
+#'
--- scripts/03_empirical_analysis/02_benthic_communities_tjarno/03_data_analysis/data_analysis_test.R
+++
@@ -1,2 +0,0 @@
-
-# data analysis scripts
