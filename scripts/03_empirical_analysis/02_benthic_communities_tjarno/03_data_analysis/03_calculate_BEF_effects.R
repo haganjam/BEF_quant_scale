696babc6d7c9df26447de3061216bb49a051bb2d
James Hagan <james_hagan@outlook.com>
2022-10-13 15:57:39 +0200

    update the model fitting script

--- scripts/03_empirical_analysis/02_benthic_communities_tjarno/03_data_analysis/03_calculate_BEF_effects.R
+++ scripts/03_empirical_analysis/02_benthic_communities_tjarno/03_data_analysis/03_calculate_BEF_effects.R
@@ -63,5 +63,170 @@ bio_env <-
   filter( !(buoy_id %in% incomplete) )
 
 # rename the variables
-bio_env
+View(bio_env)
+
+# check the correlation among these variables
+pairs(bio_env[,-c(1:3, 14:16)])
+
+# let's try build a model with three variables:
+# 1. depth
+# 2. temp_C_m
+# 3. lux_C_m
+
+# load the rethinking package
+library(rethinking)
+
+# make a data.list with the training data
+v <- bio_env[complete.cases(bio_env), ]
+head(v)
+dim(v)
+
+train.d <- 
+  list(M = (v$M - mean(v$M))/sd(v$M),
+       Y = (v$Y - mean(v$Y))/sd(v$Y),
+       TMP = (v$temp_C_m - mean(v$temp_C_m))/sd(v$temp_C_m),
+       LUX = (v$lux_m - mean(v$lux_m))/sd(v$lux_m), 
+       D = v$depth_m,
+       S = as.integer(as.factor(v$OTU))
+       )
+
+# fit the model without interactions among environmental variables
+m1 <- 
+  ulam(
+  alist(M ~ dnorm(u, sigma),
+        
+        u <- aS[S] + b_YS[S]*Y + b_TS[S]*TMP + b_LS[S]*LUX + b_DS[S]*D,
+        
+        b_YS[S] ~ normal( 0 , 1 ),
+        b_TS[S] ~ normal( 0 , 1 ),
+        b_LS[S] ~ normal( 0 , 1 ),
+        b_DS[S] ~ normal( 0, 1 ),
+        
+        aS[S] ~ normal( 0 , 2 ),
+        
+        sigma ~ dexp(1)
+        
+        ),
+  data = train.d, chains = 4, log_lik = TRUE)
+
+# check the precis output
+precis( m1 , depth=2 )
+
+# check the traceplots
+traceplot(m1)
+
+# plot the observed data versus the modelled data
+post <- sim(m1)
+plot((v$M - mean(v$M))/sd(v$M), apply(post, 2, mean))
+abline(a = 0, b = 1)
+
+# fit the model with two additional interaction terms
+m2 <- 
+  ulam(
+    alist(M ~ dnorm(u, sigma),
+          
+          u <- aS[S] + b_YS[S]*Y + b_TS[S]*TMP + b_LS[S]*LUX + b_DS[S]*D + b_YT*Y*TMP + b_YD*Y*D,
+          
+          b_YS[S] ~ normal( 0 , 1 ),
+          b_TS[S] ~ normal( 0 , 1 ),
+          b_LS[S] ~ normal( 0 , 1 ),
+          b_DS[S] ~ normal( 0, 1 ),
+          b_YT ~ normal(0, 1),
+          b_YD ~ normal(0, 1),
+          
+          aS[S] ~ normal( 0 , 2 ),
+          
+          sigma ~ dexp(1)
+          
+    ),
+    data = train.d, chains = 4, log_lik = TRUE)
+
+# check the precis output
+precis( m2, depth = 2 )
+
+# check the traceplots
+traceplot(m2)
+
+# plot the observed data versus the modelled data
+post <- sim(m2)
+plot((v$M - mean(v$M))/sd(v$M), apply(post, 2, mean))
+abline(a = 0, b = 1)
+
+# fit the model without mixture data
+m3 <- 
+  ulam(
+    alist(M ~ dnorm(u, sigma),
+          
+          u <- aS[S] + b_TS[S]*TMP + b_LS[S]*LUX + b_DS[S]*D,
+          
+          b_TS[S] ~ normal( 0 , 1 ),
+          b_LS[S] ~ normal( 0 , 1 ),
+          b_DS[S] ~ normal( 0, 1 ),
+          
+          aS[S] ~ normal( 0 , 2 ),
+          
+          sigma ~ dexp(1)
+          
+    ),
+    data = train.d, chains = 4, log_lik = TRUE)
+
+# check the precis output
+precis( m3, depth = 2 )
+
+# check the traceplots
+traceplot(m3)
+
+# plot the observed data versus the modelled data
+post <- sim(m3)
+plot((v$M - mean(v$M))/sd(v$M), apply(post, 2, mean))
+abline(a = 0, b = 1)
+
+
+# temperature and depth only with full interactions
+m4 <- 
+  ulam(
+    alist(M ~ dnorm(u, sigma),
+          
+          u <- aS[S] + b_YS[S]*Y + b_TS[S]*TMP + b_DS[S]*D + b_YTS[S]*Y*TMP + b_YDS[S]*Y*D + b_TDS[S]*TMP*D,
+          
+          b_YTS[S] ~ normal( 0 , 1 ),
+          b_YDS[S] ~ normal( 0 , 1 ),
+          b_TDS[S] ~ normal( 0 , 1 ),
+          b_YS[S] ~ normal( 0 , 1 ),
+          b_TS[S] ~ normal( 0 , 1 ),
+          b_DS[S] ~ normal( 0, 1 ),
+          
+          aS[S] ~ normal( 0 , 2 ),
+          
+          sigma ~ dexp(1)
+          
+    ),
+    data = train.d, chains = 4, log_lik = TRUE)
+
+# check the precis output
+precis( m4, depth = 2 )
+
+# check the traceplots
+traceplot(m4)
+
+# plot the observed data versus the modelled data
+post <- sim(m4)
+plot((v$M - mean(v$M))/sd(v$M), apply(post, 2, mean))
+abline(a = 0, b = 1)
+
+# compare the models using PSIS
+compare(m1, m2, m3, m4, func = PSIS)
+
+# the additional interaction terms didn't do much
+
+# check the outlying points
+post <- sim(m1)
+plot((v$M - mean(v$M))/sd(v$M), apply(post, 2, mean))
+abline(a = 0, b = 1)
+
+x1 <- (v$M - mean(v$M))/sd(v$M)
+x2 <- apply(post, 2, mean)
+
+v[(x1 < 0) & (x2 > 0.8), ] %>% View()
+
 
