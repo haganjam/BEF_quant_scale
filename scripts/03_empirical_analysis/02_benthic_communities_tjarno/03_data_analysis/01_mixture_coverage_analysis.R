1ebf9d5a8eba22014e115a71c3d8c464ff011352
James Hagan <james_hagan@outlook.com>
2022-10-17 17:31:03 +0200

    updae the analysis of the mixture cover at different time points

--- scripts/03_empirical_analysis/02_benthic_communities_tjarno/03_data_analysis/01_mixture_coverage_analysis.R
+++ scripts/03_empirical_analysis/02_benthic_communities_tjarno/03_data_analysis/01_mixture_coverage_analysis.R
@@ -15,20 +15,34 @@ library(tidyr)
 library(ggplot2)
 library(here)
 
+# load the plotting theme
+source(here("scripts/Function_plotting_theme.R"))
+
 # load the cleaned dataset
 cov_dat <- read_csv(here("data/benthic_communities_tjarno_data/data_clean/mixture_coverage_data_clean.csv"))
+
+# check how many unique buoys there are
+length(unique(cov_dat$buoy_id))
+
+# remove the species that we are not using anymore
+cov_dat <- 
+  cov_dat %>%
+  select(-all_of(c("Asci_percent", "Seasq_bumpi_percent", "Hydro_pink_percent")))
 
 # pivot this data longer
 cov_dat <- 
   cov_dat %>%
   pivot_longer(cols = ends_with("percent"),
-               names_to = "species",
+               names_to = "Species",
                values_to = "cover")
+
+# remove the percent in the words
+cov_dat$Species <- gsub(pattern = "_percent", replacement = "", x = cov_dat$Species)
 
 # select the relevant columns
 cov_dat <- 
   cov_dat %>%
-  select(buoy_id, time, species, cover)
+  select(buoy_id, time, Species, cover)
 
 # get the T1 measurements
 covT1 <- 
@@ -45,12 +59,50 @@ covT2 <-
   select(-time)
 
 # join these two together
-covT12 <- full_join(covT1, covT2, by = c("buoy_id", "species"))
+covT12 <- full_join(covT1, covT2, by = c("buoy_id", "Species"))
 
 # examine the relationship between covT1 and covT2
 plot(covT12$T1_cover, covT12$T2_cover)
 abline(a = 0, b = 1)
-cor.test(covT12$T1_cover, covT12$T2_cover, method = "spearman")
+SP.x <- cor.test(covT12$T1_cover, covT12$T2_cover, method = "spearman")
+print(SP.x)
+
+# plot a proper correlation plot
+ggplot(data = covT12,
+       mapping = aes(x = T1_cover, y = T2_cover, colour = Species)) +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed") +
+  geom_point(size = 2, alpha =0.7) +
+  scale_colour_viridis_d(option = "C") +
+  xlab("Cover (%)") +
+  ylab("Cover (%)") +
+  annotate(geom = "text", x = 10, y = 90, label = paste0("rho = ", round(SP.x$estimate, 2))) +
+  theme_meta() +
+  theme(legend.position = "top",
+        legend.key = element_rect(fill = NA))
+
+# what about total cover?
+tot_cov <- 
+  covT12 %>%
+  group_by(buoy_id) %>%
+  summarise(T1_tot_cover = sum(T1_cover, na.rm = TRUE),
+            T2_tot_cover = sum(T2_cover, na.rm = TRUE))
+
+# what is the correlation?
+SP.y <- cor(tot_cov$T1_tot_cover, tot_cov$T2_tot_cover, method = "spearman")
+
+# plot a proper correlation plot
+ggplot(data = tot_cov,
+       mapping = aes(x = T1_tot_cover, y = T2_tot_cover)) +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed") +
+  geom_point(size = 2, alpha =0.7) +
+  scale_colour_viridis_d(option = "C") +
+  xlab("Total cover (%)") +
+  ylab("Total cover (%)") +
+  annotate(geom = "text", x = 15, y = 160, label = paste0("rho = ", round(SP.y, 2))) +
+  theme_meta() +
+  theme(legend.position = "top",
+        legend.key = element_rect(fill = NA))
+
 
 # correlation within each buoy
 cor_dist <- 
@@ -62,15 +114,29 @@ cor_dist <-
 mean(cor_dist$Spearman_r, na.rm = TRUE)
 hist(cor_dist$Spearman_r)
 
-# calculate the average deviation across all species-buoy combinations
-covT12 %>%
-  mutate(T12_diff = abs((T1_cover - T2_cover)) ) %>%
-  group_by(buoy_id) %>%
-  summarise(T12_diff_m = mean(T12_diff, na.rm = TRUE),
-            T12_diff_sd = sd(T12_diff, na.rm = TRUE))
+# make a proper histogram
+ggplot(data = cor_dist,
+       mapping = aes(x = Spearman_r)) +
+  geom_histogram() +
+  scale_x_continuous(limits = c(0, 1.05)) +
+  geom_vline(xintercept = mean(cor_dist$Spearman_r, na.rm = TRUE), 
+             linetype = "dashed", colour = "red") +
+  theme_meta()
 
+# calculate the average deviation across all species-buoy combinations
+cov_diff <- 
+  covT12 %>%
+  mutate(T12_diff = abs((T1_cover - T2_cover)) )
+
+ggplot(data = cov_diff,
+         mapping = aes(x = T12_diff)) +
+  geom_histogram() +
+  geom_vline(xintercept = mean( cov_diff$T12_diff), 
+             linetype = "dashed", colour = "red" ) +
+  theme_meta()
+  
 # overall mean difference between the two panels is only 3.31
-x <- abs(covT12$T1_cover - covT12$T2_cover)
+x <- cov_diff$T12_diff
 mean(x)
 sd(x)
 
@@ -89,21 +155,6 @@ ggplot(data = covT12,
   geom_jitter(width = 0.05, shape = 1, size = 2) +
   geom_abline(intercept = 0, slope = 1, linetype = "dashed", colour = "red") +
   facet_wrap(~buoy_id) +
-  theme_bw()
-
-# what about total cover?
-tot_cov <- 
-  covT12 %>%
-  group_by(buoy_id) %>%
-  summarise(T1_tot_cover = sum(T1_cover, na.rm = TRUE),
-            T2_tot_cover = sum(T2_cover, na.rm = TRUE))
-
-# what is the correlation?
-cor(tot_cov$T1_tot_cover, tot_cov$T2_tot_cover)
-
-# plot the relationship
-plot(tot_cov$T1_tot_cover, tot_cov$T2_tot_cover, ylab = "T2_cover", xlab = "T1_cover",
-     xlim = c(0, 175), ylim = c(0, 175))
-abline(a = 0, b = 1)
+  theme_meta()
 
 ### END
