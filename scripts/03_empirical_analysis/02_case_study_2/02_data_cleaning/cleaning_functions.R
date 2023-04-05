eb2e37ada6a5d002a1028162b52866984411d9dc
Benedikt Schrofner-Brunner <84441163+bschrobru@users.noreply.github.com>
2023-04-05 17:44:06 +0200

    add description to cleaning function

--- scripts/03_empirical_analysis/02_case_study_2/02_data_cleaning/cleaning_functions.R
+++ scripts/03_empirical_analysis/02_case_study_2/02_data_cleaning/cleaning_functions.R
@@ -8,7 +8,8 @@ check.dirs = function(){
   
   library(here)
   
-  # check that the correct folder is present
+  # This function checks if the folders from the Researchbox are presents, so the cleaning scripts can run.
+  
   if(! dir.exists(here("data/case_study_2/ResearchBox 843"))){
     print("download the ResearchBox contents and run cleaning script 1")
   }
@@ -18,14 +19,30 @@ check.dirs = function(){
   }
 }
 
+#This function returns a list of filenames from the Table of Contents from Research box
 get.data.filenames = function(section.name){
 
   library(dplyr)
   library(here)
   
-  # load the .csv file with the table of contents from ResearchBox
-  table_of_contents <- read_csv(here("data/case_study_2/ResearchBox 843/Table of Contents ResearchBox 843 (peer-review).csv"))
-
+  # load the .csv file with the table of contents from ResearchBox public and peer review
+  
+    # File names
+  file_name1 <- "Table of Contents ResearchBox 843 (peer-review).csv"
+  file_name2 <- "Table of Contents ResearchBox 843 (public).csv"
+  file_path <- here("data/case_study_2/ResearchBox 843")
+  
+  if (file.exists(file.path(file_path, file_name1))) {
+    
+    table_of_contents <- read_csv(file.path(file_path, file_name1))
+    
+  } else if (file.exists(file.path(file_path, file_name2))) {
+    # If the first file doesn't exist, check if the second file exists
+    table_of_contents <- read_csv(file.path(file_path, file_name2))
+    
+  } else {stop("Table of Contents file cannot be found.")}
+  
+  
   # get file list from researchbox dir
   table_of_contents <- 
     table_of_contents %>% 
